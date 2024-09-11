ee95f5680c6d2ab64f0636b332c027a1590a3255
Anatoliy Matsakov <mactolik1993@gmail.com>
2024-09-11 16:14:58 +0300

    add user instruction with new operators

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.WriteLine("Hello! Welcome to  console calculator.\n");
         Console.WriteLine("Enter an expression in the format: number 1 operator number 2 (for example, 2 + 2).\nTo perform an operation on a non-integer number, you must enter it with the \",\" or \".\" symbol.");
-        Console.WriteLine("Supported operators: +, -, *, /,^");
+        Console.WriteLine("Supported operators: +, -, *, /,r(root,just write r after digit),%(remainder of division)");
         Console.WriteLine("To get the result after the expression, press equals(=)");
         Console.WriteLine("Press Esc to exit app");
         Console.WriteLine("Write an expression. If you made a mistake with your input, you can delete it by pressing the backspace button\n");
