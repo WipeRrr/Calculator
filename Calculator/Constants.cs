be985f56f79265586805430eeb8bde8842066ba5
Anatoliy Matsakov <mactolik1993@gmail.com>
2024-09-12 15:55:54 +0300

    edit deadlines

--- Calculator/Constants.cs
+++ Calculator/Constants.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-
+﻿
 namespace Calculator
 {
     public static class Constants
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,7 +1,5 @@
 ﻿namespace Calculator;
 
-
-
 class Program
 {
     static void Main(string[] args)
@@ -36,43 +34,3 @@ class Program
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class Program
-//{
-//    static void Main()
-//    {
-//        Console.WriteLine("Hello! Welcome to  console calculator.\n");
-//        Console.WriteLine("Enter an expression in the format: number 1 operator number 2 (for example, 2 + 2).\nTo perform an operation on a non-integer number, you must enter it with the \",\" or \".\" symbol.");
-//        Console.WriteLine("Supported operators: +, -, *, /,r(root,just write r after digit),%(remainder of division)");
-//        Console.WriteLine("To get the result after the expression, press equals(=)");
-//        Console.WriteLine("Press Esc to exit app");
-//        Console.WriteLine("Write an expression. If you made a mistake with your input, you can delete it by pressing the backspace button\n");
-
-//        var operation = new OperationsWithCalculator();
-//        var getExpression = new ExpressionReceiver();
-
-//        while (true)
-//        {
-//            //Отслеживаем что вводит пользователь и записываем результат в переменную
-//            var input = getExpression.GetExpressionFromUser();
-
-//            //Разбиваем строку на 2 части с помощью  методов массива и проводим математическую операцию
-//            var result = operation.DoMathOperations(input);
-//            Console.WriteLine(result);
-//        }
-
-//    }
-//}
